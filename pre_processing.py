327f137ac2e8d6c403b3c1f53ff54c0ad19cbb18
Ai Makabi <a.macbee@gmail.com>
2015-10-28 19:24:35 +0900

    準備のための諸々追加

---
+++ pre_processing.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from sklearn.datasets import fetch_mldata
+import gzip
+import numpy as np
+import pickle
+
+
+def prepare_mnist_data():
+    """
+    MNIST dataset をダウンロードする処理
+    :param train_N: 学習用データの個数 (残りをテスト用とする)
+    :return:
+    """
+
+    with gzip.open('./data/mnist.pkl.gz', 'rb') as f:
+        train, valid, test = pickle.load(f)
+
+    np.savez('./data/mnist', x_train=train[0], x_test=test[0], y_train=train[1], y_test=test[1])
+
+
+if __name__ == "__main__":
+    prepare_mnist_data()
